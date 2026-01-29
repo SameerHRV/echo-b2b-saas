12e01b72f09811a9ff8889cb53c95ad859194559
SameerHRV <your_github_username@users.noreply.github.com>
2026-01-29 11:27:17 +0530

    00: Setup App

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,13 @@
-import { Button } from "@workspace/ui/components/button";
 import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
+import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">App/Hello World</h1>
+        <h1 className="text-2xl font-bold">Widgets/hello worls</h1>
         <Button size="sm">Button</Button>
-        <Input placeholder="Enter anythingg ..." />
-        <p>{add(558, 14854)}</p>
+        {add(1, 2)}
       </div>
     </div>
   );
